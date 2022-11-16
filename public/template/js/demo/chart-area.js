66066eea153635f8b3da9122a0b95868f675d8ab
jackiee23 <adil.abdulhaq2309@gmail.com>
2022-11-17 00:38:25 +0700

    fix mata uang

--- public/template/js/demo/chart-area.js
+++ public/template/js/demo/chart-area.js
@@ -34,7 +34,7 @@ var myLineChart = new Chart(ctx, {
   data: {
     labels: nama_bulan,
     datasets: [{
-      label: "Earnings",
+      label: "Omset",
       lineTension: 0.3,
       backgroundColor: "rgba(78, 115, 223, 0.05)",
       borderColor: "rgba(78, 115, 223, 1)",
@@ -78,7 +78,7 @@ var myLineChart = new Chart(ctx, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return 'Rp' + number_format(value) +',00';
           }
         },
         gridLines: {
@@ -110,7 +110,7 @@ var myLineChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ': Rp' + number_format(tooltipItem.yLabel)+',00';
         }
       }
     }
